44ccc3368cb62db3de339b68466e830d0641e6d9
Igor-Belokon <igorbelokon8@gmail.com>
2019-11-23 22:02:06 +0200

    come on

---
+++ src/components/prof/prof.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style.css";
+
+import { connect } from "react-redux";
+
+import { Redirect } from "react-router-dom";
+
+import { regUser } from "../../store/action/data";
+
+export default class MyProf extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
